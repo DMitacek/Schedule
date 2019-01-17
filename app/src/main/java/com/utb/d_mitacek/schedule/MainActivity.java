e7024ee49cf3d978f5b6ae7efc52a9f19e318f02
DMitacek <DMitacek@seznam.cz>
2019-01-17 20:13:00 +0100

    Item class created

--- app/src/main/java/com/utb/d_mitacek/schedule/MainActivity.java
+++ app/src/main/java/com/utb/d_mitacek/schedule/MainActivity.java
@@ -1,12 +1,16 @@
 package com.utb.d_mitacek.schedule;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
