8eadb5b9938b7afd587f8888e7ee427e50d7879e
DMitacek <DMitacek@seznam.cz>
2019-01-18 15:26:27 +0100

    RecycleView Adapter Added

---
+++ app/src/main/java/com/utb/d_mitacek/schedule/ScheduleAdapter.java
@@ -0,0 +1,53 @@
+package com.utb.d_mitacek.schedule;
+
+        import android.support.v7.widget.RecyclerView;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        import java.util.ArrayList;
+
+
+public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder> {
+    private ArrayList<ScheduleItem> mScheduleList;
+
+    public static class ScheduleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public ScheduleViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public ScheduleAdapter(ArrayList<ScheduleItem> scheduleList) {
+        mScheduleList = scheduleList;
+    }
+
+    @Override
+    public ScheduleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.schedule_item, parent, false);
+        ScheduleViewHolder evh = new ScheduleViewHolder(v);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ScheduleViewHolder holder, int position) {
+        ScheduleItem currentItem = mScheduleList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mScheduleList.size();
+    }
+}
